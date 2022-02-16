f09bbf74f4e06154ea7ca9b46b492c69ea0ff399
William <wcong521@gmail.com>
2022-02-16 13:23:43 -0600

    Fix

--- src/uiControl.js
+++ src/uiControl.js
@@ -8,8 +8,8 @@ export class UiControl {
 
         // default settings for UI elements
 
-        this.FONT_FAMILY = '../src/assets/Roboto-msdf.json';
-        this.FONT_TEXTURE = '../src/assets/Roboto-msdf.png';
+        this.FONT_FAMILY = '../node_modules/three-mesh-ui/examples/assets/Roboto-msdf.json';
+        this.FONT_TEXTURE = '../node_modules/three-mesh-ui/examples/assets/Roboto-msdf.png';
 
         this.DEFAULT_CONTAINER = {
             width: 1.2,
